8d16f0e5e225d821a16188abd335674fa52dc556
d4rev <un.finished342@slmail.me>
2024-11-24 21:10:52 +0800

    TODO on src/Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// TODO: @Zeki-Zek  add ur code here
+//                  also add other .cs files
