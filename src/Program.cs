c745e155dc087725ba84db6ad30c5f4da889e0e8
d4rev <un.finished342@slmail.me>
2024-11-24 22:09:30 +0800

    Revert "TODO on src/Program.cs"
    This reverts commit 8d16f0e5e225d821a16188abd335674fa52dc556.

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// TODO: @Zeki-Zek  add ur code here
-//                  also add other .cs files
+Console.WriteLine("Hello, World!");
