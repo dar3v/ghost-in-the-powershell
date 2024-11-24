166fdde296a25313132b3ba86da0efaab144e234
d4rev <un.finished342@slmail.me>
2024-11-24 22:59:03 +0800

    rm .gitignored files

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghost_in_The_PowerShell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game mygame = new Game();
+            mygame.Start();
+        }
+    }
+}
