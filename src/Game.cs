49fa699e42749c822721ea0e9bb11ae52519c5d5
Zeki-Zek <zekiiipoesia@yahoo.com>
2024-11-26 14:19:05 +0800

    Install NAudio

--- src/Game.cs
+++ src/Game.cs
@@ -6,7 +6,7 @@ using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-using System.Windows.Extensions; //FIXME
+
 
 
 
--- src/Menu.cs
+++ src/Menu.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-using System.Windows.Extensions; //FIXME
+
 
 namespace Ghost_in_The_PowerShell
 {
