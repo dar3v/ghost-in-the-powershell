0276a82834130303b4b8fb5dab82f1529d3b2916
d4rev <un.finished342@slmail.me>
2024-12-19 12:06:03 +0800

    init `MazeGame.cs`

---
+++ src/Engine/MazeGame.cs
@@ -0,0 +1,47 @@
+using RaycasterCS;
+
+namespace MazeGame
+{
+    class InitMazeGame()
+    {
+        internal void StartMenu()
+        {
+            // TODO: add something better here
+            Console.WriteLine("Maze Game start menu");
+        }
+    }
+
+    class ScaryMaze
+    {
+        // TODO: add the actual level here
+        public void LevelOne()
+        {
+            InitGame.MazeMap = [
+                  "###############",
+                  "#.............#",
+                  "#.#####..####.#",
+                  "#.#.........#.#",
+                  "#.#.###..##.#.#",
+                  "#.#.#.....#.#.#",
+                  "#.#.#.#.#.#.#.#",
+                  "#.....#.#.....#",
+                  "#.....#.#.....#",
+                  "#.#.#.....#.#.#",
+                  "#.#.###..##.#.#",
+                  "#.#.........#.#",
+                  "#.#####..####.#",
+                  "#.............#",
+                  "###############",
+            ];
+
+            InitGame.initMapWidth = 16;
+            InitGame.initMapHeight = 16;
+            InitGame.initPlayerX = 1;
+            InitGame.initPlayerY = 1;
+
+            // --ENGINE-- start
+            Engine Engine = new();
+            Engine.Start();
+        }
+    }
+}
